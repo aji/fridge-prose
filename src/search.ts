b8a0a25e1ac439dfb5b9b0d92f1d31e5723099b7
Alex Iadicicco <alex@ajitek.net>
2023-12-31 14:05:23 -0800

    Allow multiple search terms basically

--- src/search.ts
+++ src/search.ts
@@ -3,6 +3,11 @@ import phrasesRaw from './phrases.txt?raw';
 const phrases = phrasesRaw.split('\n').filter((x) => x);
 
 export function search(query: string): string[] {
-  const q = query.toLowerCase();
-  return phrases.filter((s) => s.toLowerCase().includes(q));
+  const qs = query
+    .trim()
+    .toLowerCase()
+    .split(' ')
+    .map((x) => x.trim())
+    .filter((x) => x);
+  return phrases.filter((s) => qs.every((q) => s.toLowerCase().includes(q)));
 }
