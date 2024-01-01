4699a6b5028e37b344f174fbaf7beba3dd56a416
Alex Iadicicco <alex@ajitek.net>
2023-12-31 16:56:54 -0800

    Reorder imports

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { AppBoardElement } from './AppBoardElement.ts';
 import { AppBoardSelectionElement } from './AppBoardSelectionElement.ts';
 import { AppDrawerElement } from './AppDrawerElement.ts';
 import { AppDrawerOptionsElement } from './AppDrawerOptionsElement.ts';
+import { AppMenu } from './AppMenu.ts';
+import { AppModalElement } from './AppModalElement.ts';
 import { AppSnapRule } from './AppSnapRule.ts';
 import { AppTileElement } from './AppTileElement.ts';
-import { AppModalElement } from './AppModalElement.ts';
 import './index.css';
-import { AppMenu } from './AppMenu.ts';
 
 AppBoardElement.register();
 AppBoardSelectionElement.register();
